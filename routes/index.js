7be208b99f3495fa3b306c9a0f347c619e209fa8
TomB-S <147733386+TomB-S@users.noreply.github.com>
2024-02-07 21:32:15 +0100

    Backend updated

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,10 @@ const fetch = require('node-fetch');
 const TMDB_API_KEY  = process.env.TMDB_API_KEY;
 
 router.get('/movies', (req, res) => {
-        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_API_KEY }`)
+        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_API_KEY}`)
         .then(response => response.json())
         .then( data => {
-            res.json({ movies : data.results });
+            res.json({ movies : data.results }); // reponse qui contient un objet avec la propriété movies
         }).catch(err => console.error('error:' + err))
     });
 
