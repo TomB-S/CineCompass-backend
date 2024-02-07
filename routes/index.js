4631bc8473b9c5917f83611f26c45c5c21df9f47
TomB-S <147733386+TomB-S@users.noreply.github.com>
2024-02-07 17:30:26 +0100

    route APIbackend has be done and workind

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,21 @@
+require('dotenv').config();
 var express = require('express');
 var router = express.Router();
+//importing the module 
+const fetch = require('node-fetch');
+
+// defining my api key 
+const TMDB_API_KEY = process.env.TMDB_API_KEY;
+
+// creating the route 
+router.get('/movies', (req,res) => {
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_API_KEY}`)
+    then(response => response.json())
+    then(data => {
+        res.json({ movies : data.results })
+    });
+});
+
+
 
 module.exports = router;
